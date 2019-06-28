6edc6b16025c840cd228f133754d693740363e12
martin <martinastruc@gmail.com>
2019-06-28 12:09:00 +0200

    test

--- src/generator.js
+++ src/generator.js
@@ -1,9 +1,10 @@
 function createBitcoin() {
-    console.log('Loading ...')
+    console.log('Loading ...');
 
     setTimeout(() => console.log('Created !'), 1000);
 
     let varToRemove = true;
+    varToRemove = true;
 }
 
 module.exports = {
